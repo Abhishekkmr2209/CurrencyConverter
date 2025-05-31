69c320dec9c8f4d33514d253001221b81013cfcf
Abhishek Kumar <abhishekkmr.2209@gmail.com>
2025-05-31 23:57:59 +0530

    Functionality completed,progress to styling

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const access_key = "33a09fccd2d93ca0c8b38c5850b45473";
-const URL =`https://api.currencylayer.com/convert?`;
+const BASE_URL =`https://api.currencylayer.com/convert?`;
 
 /* access_key , from , to and amount are  required in querystring*/
 
 const dropDown = document.querySelector('.dropDown');
 const selectElements = document.querySelectorAll('select');
-
+const from = document.querySelector('select[name="From"]');
+const to = document.querySelector('select[name="To"]');
+const msg=document.querySelector('.msg');
+const btn = document.querySelector('.button');
 
 
 for(let select of selectElements){
@@ -30,16 +33,28 @@ function updateFlag(evt){
 
 
 
-function amountReset(){
+
+
+async function amountExchangeRate(){
     let amount = document.querySelector('#amount');
     let amtValue = amount.value;
     if(amtValue==="" || amtValue<1){
         amount.value=1;
 
     }
+    const URL = `${BASE_URL}&access_key=${access_key}&from=${from.value}&to=${to.value}&amount=${amount.value}`;
+    let response = await fetch(URL,{
+        method:"GET"
+    });
+    let rate = await response.json();
+    let msgDisplayPara = msg.querySelector('p');
+    msgDisplayPara.innerHTML = `1${from.value} = ${rate.result}${to.value}<br/> ${amtValue}${from.value} converts to ${rate.result*amtValue}${to.value}`;
 }
 
+btn.addEventListener('click',(evt)=>{
+     evt.preventDefault();
+    amountExchangeRate();
+});
 
 
-
-window.addEventListener('load',amountReset());
+window.addEventListener('load',amountExchangeRate());
